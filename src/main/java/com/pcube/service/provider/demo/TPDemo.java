d20c5537e655a08bd2932207bd6f5cdd9efdd40b
linshuguang <linshuguang@xiaomi.com>
2019-01-28 17:56:55 +0800

    refactor

--- src/main/java/com/pcube/service/provider/demo/TPDemo.java
+++ src/main/java/com/pcube/service/provider/demo/TPDemo.java
@@ -3,10 +3,12 @@ package com.pcube.service.provider.demo;
 import com.pcube.service.TRequest;
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * Created by kenya on 2019/1/26.
  */
 @Data
-public class TPDemo extends TRequest {
+public class TPDemo extends TRequest implements Serializable {
     String demo;
 }
--- src/main/java/com/pcube/service/provider/demo/TRDemo.java
+++ src/main/java/com/pcube/service/provider/demo/TRDemo.java
@@ -3,10 +3,12 @@ package com.pcube.service.provider.demo;
 import com.pcube.service.TResponse;
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * Created by kenya on 2019/1/26.
  */
 @Data
-public class TRDemo extends TResponse {
+public class TRDemo extends TResponse implements Serializable {
     String demo;
 }

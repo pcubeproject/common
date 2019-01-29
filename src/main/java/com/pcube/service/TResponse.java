d4c4b415cc57afc6323227ca018ea5b3cf14eb7e
linshuguang <linshuguang@xiaomi.com>
2019-01-29 09:29:42 +0800

    response

--- src/main/java/com/pcube/service/TResponse.java
+++ src/main/java/com/pcube/service/TResponse.java
@@ -1,9 +1,14 @@
 package com.pcube.service;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * Created by kenya on 2019/1/24.
  */
+@Data
 public class TResponse implements Serializable {
+    Object result;
+    boolean success;
 }
